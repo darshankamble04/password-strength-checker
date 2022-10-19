d750b153feb1959625c1e8d3a1be2630fd8cad1d
darshankamble04 <darshankamble7371@gmail.com>
2022-10-19 12:04:28 +0530

    convert logical into Regular expression

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Bookmark } from 'react-bookmark';
+// import { Bookmark } from 'react-bookmark';
 import './App.css';
 import Loader from './components/loader/Loader';
 
@@ -12,47 +12,37 @@ function App() {
   const [isLowercase, setIsLowercase] = useState(false);
   const [isNumber, setIsNumber] = useState(false);
   const [isSymbol, setIsSymbol] = useState(false);
-  const [isLoad, setIsLoad] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const toggleChange = (e) => {
     const password = e.target.value;
-
-    // console.log(password.length)
-    let character = '';
     let flag1 = 1;
     let flag2 = 1;
     let flag3 = 1;
+    let flag4 = 1;
+    let flag5 = 1;
 
-    for (let i = 0; i <= password.length; i++) {
-      character = password.charAt(i);
-
-      if (isNaN(character * 1)) {
-        if (character == character.toUpperCase()) {
-          // console.log('upper case true');
-          setIsUppercase(true)
-          flag1 = 0;
-        }
-        if (character == character.toLowerCase()) {
-          // console.log('lower case true');
-          setIsLowercase(true)
-          flag2 = 0;
-        }
-      }
-      if (i >= 8) {
-        setPwLength(true)
-        flag3 = 0;
-      }
-      console.log(character)
-      if(!isNaN(character)){
-        console.log(character + " is a number <br/>");
-       }
+    if (password.match(/[0-9]/)) {
+      setIsNumber(true)
+      flag5 = 0;
     }
-
-    // console.log(isNumber(character))
-    // console.log(_.isNumber(character))
-    // if(Number(character)){
-    //   console.log("Num is entereed")
-    // }
+    if (password.match(/[a-z]/)) {
+      setIsLowercase(true)
+      flag2 = 0;
+    }
+    if (password.match(/[A-Z]/)) {
+      setIsUppercase(true)
+      flag1 = 0;
+    }
+    if (password.match(/([!,#,$,%,&,*,+,/,=,>,?,@,^,_,',~,])/)) {
+      setIsSymbol(true);
+      flag4 = 0;
+    }
+    if (password.length >= 8) {
+      setPwLength(true)
+      flag3 = 0;
+    }
+    
     if (flag1 === 1) {
       setIsUppercase(false)
     }
@@ -62,19 +52,23 @@ function App() {
     if (flag3 === 1) {
       setPwLength(false)
     }
+    if (flag4 === 1) {
+      setIsSymbol(false)
+    }
+    if (flag5 === 1) {
+      setIsNumber(false)
+    }
+
+    setIsDisabled(pwLength && isUppercase && isLowercase && isNumber && isSymbol)
   }
 
   return (
-    <div>
+    <div className='Ccontainer'>
       {/* <div className={`${isLoad ? "d-none" : ""}`}>
         <Loader />
       </div> */}
-      <Bookmark className="coolClass" href="/cool/path" title="My Cool Website" />
-      <form className="form container" action="">
-        {/* <div>
-        <label style={{ paddingRight: "20px" }} >Password</label>
-        <input  autoFocus type="text" />
-      </div> */}
+      {/* <Bookmark className="coolClass" href="/cool/path" title="My Cool Website" /> */}
+      <form onLoad={()=>{console.log("load")}} className="form" action="">
         <div className="row g-3 align-items-center">
           <div className="col-auto">
             <label className="col-form-label">Password</label>
@@ -87,13 +81,14 @@ function App() {
         <div className="checkPoints my-3 d-grid flex-column">
           <span className="">Password requirements : </span>
 
-          <span className={`point ${isUppercase ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isUppercase ? "done" : "noise_control_off"}</span> At Least one CAPITAL letter</span>
-          <span className={`point ${isLowercase ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isLowercase ? "done" : "noise_control_off"}</span> At least one small letter</span>
-          <span className={`point ${isNumber ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isNumber ? "done" : "noise_control_off"}</span> At least one number</span>
-          <span className={`point ${isSymbol ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isSymbol ? "done" : "noise_control_off"}</span> Include at least one of the following special characters: {`!#$%&*+/=>?@][^_ '~{|}`}</span>
-          <span className={`point ${pwLength ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{pwLength ? "done" : "noise_control_off"}</span> Be at least 8 charactors</span>
+          <span className={`point ${isUppercase ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isUppercase ? "done" : "noise_control_off"}</span> AtLeast one CAPITAL letter</span>
+          <span className={`point ${isLowercase ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isLowercase ? "done" : "noise_control_off"}</span> Atleast one small letter</span>
+          <span className={`point ${isNumber ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isNumber ? "done" : "noise_control_off"}</span> Atleast one number</span>
+          <span className={`point ${isSymbol ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{isSymbol ? "done" : "noise_control_off"}</span> Atleast one of the following special characters: {`!#$%&*+/=>?@][^_ '~{|}`}</span>
+          <span className={`point ${pwLength ? "detect" : ""} my-1 mx-4 d-flex align-items-center`}> <span className="material-icons mx-1 fw-bolder">{pwLength ? "done" : "noise_control_off"}</span> Atleast 8 characters</span>
 
         </div>
+        <button type="button" disabled={isDisabled} class="btn btn-primary">Submit</button>
       </form>
     </div>
   );
